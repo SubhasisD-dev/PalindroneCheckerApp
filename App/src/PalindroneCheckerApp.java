8188e18f998dada757f2f2be19e0dbfa1c19deb0
SubhasisD-dev <sd2033@srmist.edu.in>
2026-02-26 16:18:35 +0530

    [Subhasis Dutta] Added >> >> [usecase6]

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,22 +1,31 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindroneCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "radar";
+        String input = "level";
         Stack<Character> stack = new Stack<>();
-        String reversed = "";
+        Queue<Character> queue = new LinkedList<>();
 
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
         }
+
+        boolean isPalindrome = true;
 
         while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+            if (!stack.pop().equals(queue.remove())) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        if (input.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("The string \"" + input + "\" is a PALINDROME.");
         } else {
             System.out.println("The string \"" + input + "\" is NOT a palindrome.");
